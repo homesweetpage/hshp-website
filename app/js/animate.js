2cd15b740696148d2523d00c004af96c1c33313b
jisegura <segurajuanignacio@outlook.com>
2017-03-15 19:02:03 -0300

    add us animation && result animation

--- app/js/animate.js
+++ app/js/animate.js
@@ -1,2 +1,41 @@
 'use strict';
 
+var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+function animateResult() {
+  var $result = $('.result');
+  var $boxImg = $result.children('.box-img');
+  var $img = $boxImg.children('img');
+  $boxImg.removeClass('hidden').addClass('fadeIn');
+  $img.each(function(index, el) {
+    setTimeout(function() {
+      $(el).removeClass('hidden').addClass('zoomIn');
+    }, 150*index);
+  });
+}
+
+function animateUs() {
+  var $us = $('.us');
+  var $boxImg = $us.children('.box-img');
+  $boxImg.removeClass('hidden').addClass('fadeIn').one(animationEnd, function() {
+    animateResult();
+  });
+}
+
+function animateIdea() {
+  var $idea = $('.idea');
+  var $boxImg = $idea.children('.box-img');
+  $boxImg.removeClass('hidden').addClass('fadeIn').one(animationEnd, function() {
+    animateUs();
+  });
+}
+
+function animateHome() {
+  animateIdea(); // Start animate in serie
+}
+
+function animateReady() {
+  animateHome();
+}
+
+$(document).ready(animateReady);
